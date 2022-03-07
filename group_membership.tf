92eabb598fcb2c30334f432d4387a0aee7147a19
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-03-07 14:20:11 -0500

    Move group membership handling into its own file

---
+++ group_membership.tf
@@ -0,0 +1,11 @@
+# Put the users in the IAM group that gives them permission to read/write
+# the findings data S3 bucket.
+resource "aws_iam_user_group_membership" "user" {
+  for_each = toset(var.users)
+
+  user = data.aws_iam_user.users[each.value].user_name
+
+  groups = [
+    aws_iam_group.findings_data_s3_users.name,
+  ]
+}
--- users.tf
+++ users.tf
@@ -4,15 +4,3 @@ data "aws_iam_user" "users" {
 
   user_name = each.value
 }
-
-# Put the users in the IAM group that gives them permission to read/write
-# the findings data S3 bucket.
-resource "aws_iam_user_group_membership" "user" {
-  count = length(var.users)
-
-  user = data.aws_iam_user.users[count.index].name
-
-  groups = [
-    aws_iam_group.findings_data_s3_users.name,
-  ]
-}
